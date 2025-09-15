38d95589c13e83820f23d6b5c9f1138ef71e419e
torvalds <torvalds@linux-foundation.org>
2025-09-14 21:47:11 -0700

    testing

--- lib/rtfw/ws/handlers/receive.ex
+++ lib/rtfw/ws/handlers/receive.ex
@@ -18,13 +18,25 @@ defmodule Rtfw.Ws.Handlers.Receive do
     cond do
       op == 10 ->
         heartbeat_interval = data |> Map.get("d") |> Map.get("heartbeat_interval")
-        IO.puts "HEARTBEAT. ms: #{heartbeat_interval}"
+        manage_heartbeat(heartbeat_interval, state)
+      
+      op == 11 ->
+        IO.puts "Heartbeat ACK from Discord."
 
       true ->
         IO.puts "Rtfw.WS.Handlers.Receive: Unhandled OP code: #{op}"
 
     end
 
+    {:ok, state}
+  end
+
+  defp manage_heartbeat(interval, state) do 
+    # https://discord.com/developers/docs/events/gateway#heartbeat-interval
+    jitter = :rand.uniform()
+    Task.async(fn ->
+      :timer.sleep(interval * jitter)
+    end)
     {:ok, state}
   end
 end
