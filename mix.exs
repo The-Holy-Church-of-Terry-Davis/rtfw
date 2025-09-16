e0cd70fd254726d20551abb14146fcddab20d331
torvalds <torvalds@linux-foundation.org>
2025-09-16 15:07:04 -0700

    Added releases to mix.exs

--- mix.exs
+++ mix.exs
@@ -7,7 +7,13 @@ defmodule Rtfw.MixProject do
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      releases: [
+        dbot_rtfw: [
+          include_executables_for: [:unix],
+          applications: [runtime_tools: :permanent]
+        ]
+      ]
     ]
   end
 
